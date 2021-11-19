b60f0528a8eb16f68661142118e6157a5b0e7373
goldenyak <goldenyakegor@gmail.com>
2021-11-19 23:25:48 +0300

    add some functions

--- my-app/src/pages/SignIn.tsx
+++ my-app/src/pages/SignIn.tsx
@@ -1,21 +1,47 @@
 import { makeStyles } from '@material-ui/core';
 import React from 'react';
 import theme from "../theme";
+import {Typography} from "@mui/material";
 
 const useStyles = makeStyles ((theme) => ({
+    wrapper: {
+        display: 'flex',
+        height: '100vh',
+    },
+    blueSide: {
+        backgroundColor: '#1da1f2',
+        flex: '50%',
+        listStyleType: 'none',
+        color: 'white',
+        fontSize: '34px'
+
+    },
+    loginSide: {
+        flex: '50%',
+    },
 
 }));
 
 
 function SignIn() {
-
     const classes = useStyles();
+
     return (
-        <div style={{display: 'flex'}}>
-            <div style={{backgroundColor: 'red', height: '300px', flex: '50%'}}>
-                hello
+        <div className={classes.wrapper}>
+            <div className={classes.blueSide}>
+                <ul>
+                    <li>
+                        <Typography>Читайте о том, что вам интересно.</Typography>
+                    </li>
+                    <li>
+                        <Typography>Узнайте то, о чем говорят в мире.</Typography>
+                    </li>
+                    <li>
+                        <Typography>Присоединяйтесь к общению.</Typography>
+                    </li>
+                </ul>
             </div>
-            <div style={{backgroundColor: 'blue', height: '500px', flex: '50%'}}>
+            <div className={classes.loginSide}>
                 world
             </div>
         </div>
