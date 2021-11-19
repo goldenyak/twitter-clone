f93713a714fc0905526b6ed24b2693b6415b5f7b
goldenyak <goldenyakegor@gmail.com>
2021-11-19 22:13:21 +0300

    add some functions

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,10 +1,26 @@
 import React from 'react';
 import './App.css';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+
+const divStyle = {
+    marginBottom: '20px',
+    color: 'red',
+
+}
+
 
 function App() {
     return (
         <div className="App">
-            hello world
+            <div style={{marginBottom: '50px', color: 'blue', textDecoration: 'line-through'}}>hello world</div>
+            <button style={divStyle}>click me</button>
+            <div>
+                <Button variant="outlined">Outlined</Button>
+            </div>
+            <div>
+                <TextField label="Введите" variant="standard" />
+            </div>
         </div>
     );
 }
