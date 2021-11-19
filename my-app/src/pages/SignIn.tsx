194e8c2e262be1510e44adad2e1e2fb0c10bb384
goldenyak <goldenyakegor@gmail.com>
2021-11-19 23:02:51 +0300

    add some functions

--- my-app/src/pages/SignIn.tsx
+++ my-app/src/pages/SignIn.tsx
@@ -1,10 +1,23 @@
+import { makeStyles } from '@material-ui/core';
 import React from 'react';
+import theme from "../theme";
+
+const useStyles = makeStyles ((theme) => ({
+
+}));
 
 
 function SignIn() {
+
+    const classes = useStyles();
     return (
-        <div>
-            hello
+        <div style={{display: 'flex'}}>
+            <div style={{backgroundColor: 'red', height: '300px', flex: '50%'}}>
+                hello
+            </div>
+            <div style={{backgroundColor: 'blue', height: '500px', flex: '50%'}}>
+                world
+            </div>
         </div>
     );
 }
