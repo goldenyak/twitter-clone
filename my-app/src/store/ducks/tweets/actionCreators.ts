708dcb3e24722b5fa8c7cbe728b0739ef14cc230
goldenyak <goldenyakegor@gmail.com>
2021-12-06 22:44:31 +0300

    add some functions

--- my-app/src/store/ducks/tweets/actionCreators.ts
+++ my-app/src/store/ducks/tweets/actionCreators.ts
@@ -3,9 +3,15 @@ import {TweetsState} from "./contracts/state";
 
 export enum TweetsActionType {
     SET_TWEETS = "tweets/SET_TWEETS",
+    FETCH_TWEETS = "tweets/FETCH_TWEETS",
 }
 
 export interface SetTweetsActionInterface extends Action<TweetsActionType> {
+    type: TweetsActionType.SET_TWEETS,
+    payload: TweetsState['items'],
+}
+
+export interface FetchTweetsActionInterface extends Action<TweetsActionType> {
     type: TweetsActionType.SET_TWEETS,
     payload: TweetsState['items'],
 }
