330ae15b866c297005c778c6b87725bd222b2e3e
goldenyak <goldenyakegor@gmail.com>
2021-12-07 00:27:40 +0300

    add redux functions

--- my-app/src/store/store.ts
+++ my-app/src/store/store.ts
@@ -3,11 +3,16 @@ import createSagaMiddleware from "redux-saga";
 import {rootReducer} from "./rootReducer";
 import rootSaga from "./saga";
 
+declare global {
+    interface Window {
+        __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose
+    }
+}
 
 const composeEnhancers = (typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
 
 const sagaMiddleware = createSagaMiddleware()
 
-sagaMiddleware.run(rootSaga)
-
 export const store = createStore(rootReducer, composeEnhancers(applyMiddleware(sagaMiddleware)));
+
+sagaMiddleware.run(rootSaga)
