d0c08c60a9371899973fe8db64fb8a52ec03e422
goldenyak <goldenyakegor@gmail.com>
2021-12-09 21:57:35 +0300

    add some functions

--- my-app/src/store/ducks/tweets/selectors.ts
+++ my-app/src/store/ducks/tweets/selectors.ts
@@ -1,3 +1,5 @@
 import {TweetsState} from "./contracts/state";
 
 export const selectTweets = (state: TweetsState) => state.items;
+
+export const selectLoadingState = (state: TweetsState) => state.loadingState;
