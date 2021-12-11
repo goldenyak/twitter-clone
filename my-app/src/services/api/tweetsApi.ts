52ff0a4110df19b487f9267f1a6230b0435b7dce
goldenyak <goldenyakegor@gmail.com>
2021-12-11 23:12:17 +0300

    add json files

--- my-app/src/services/api/tweetsApi.ts
+++ my-app/src/services/api/tweetsApi.ts
@@ -3,6 +3,6 @@ import {TweetsState} from "../../store/ducks/tweets/contracts/state";
 
 export const TweetsApi = {
     fetchTweets(): Promise<TweetsState['items']> {
-        return axios.get('https://trycode.pw/c/2OBQ1.json').then(({ data }) => data)
+        return axios.get('/tweets').then(({ data }) => data)
     }
 }
